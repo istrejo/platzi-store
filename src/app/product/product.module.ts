80fdc8361ba1d5418de2f947023a47b98bba9615
istrejo <istrejo2106@gmail.com>
2021-04-29 21:48:34 -0400

    Instalando Angular Material

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ProductRoutingModule } from './product-routing.module';
 import { ProductsComponent } from './components/products/products.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
+import { ProductRoutingModule } from './product-routing.module';
+import { MaterialModule } from './../material/material.module';
+
 @NgModule({
   declarations: [ProductsComponent, ProductComponent, ProductDetailComponent],
-  imports: [CommonModule, ProductRoutingModule],
+  imports: [CommonModule, ProductRoutingModule, MaterialModule],
 })
 export class ProductModule {}
