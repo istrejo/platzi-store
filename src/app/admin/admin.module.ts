0a0250b069b7d2eac91cd92a3821894d742719b6
istrejo <istrejo2106@gmail.com>
2021-05-18 23:35:04 -0400

    Formulario de productos

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -11,6 +11,7 @@ import { TableComponent } from './components/table/table.component';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
+import { FormProductComponent } from './components/form-product/form-product.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { ProductsListComponent } from './components/products-list/products-list.
     TableComponent,
     DashboardComponent,
     ProductsListComponent,
+    FormProductComponent,
   ],
   imports: [
     CommonModule,
